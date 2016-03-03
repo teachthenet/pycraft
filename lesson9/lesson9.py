3930b2cbb3cba27c8ee6372962aa87c324be44e8
emeth- <emeth@Seans-MacBook-Pro.local>
2016-03-03 08:45:59 -0600

    added lesson 9

---
+++ lesson9/lesson9.py
@@ -0,0 +1,9 @@
+import mcpi.minecraft as minecraft
+mc = minecraft.Minecraft.create(address="162.244.165.151", name="seanybob") #NOTE - replace "seanybob" with your name
+
+
+#CHALLENGE 
+# Take an image as input, and read it pixel by pixel.
+# For each pixel in the image, set a block in the minecraft world closest to the color of that pixel.
+# End result should be pixel art in minecraft world.
+# Use mario.gif in this directory to start with.
