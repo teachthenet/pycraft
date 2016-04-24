d8a748827f678026f25c2305b9a938e92cbef309
emeth- <seanybob@gmail.com>
2016-04-24 15:52:55 -0500

    modify script

--- script.py
+++ script.py
@@ -8,12 +8,3 @@ y = 110
 z = 12
 
 mc.player.setPos(x, y, z)
-
-
-
-
-#CHALLENGE 1
-# Modify the script above to go to a different x/y/z location.
-
-#CHALLENGE 2
-# Figure out which of the three coordinates (x, y, or z) controls how high up you are in the air.
